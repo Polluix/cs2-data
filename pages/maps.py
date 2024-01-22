79ddaeac6c61e6e5e757efbe14cdd6c2f4a95407
Polluix <lipe311973@gmail.com>
2024-01-22 10:46:41 -0300

    changed dataframe name to df

--- pages/maps.py
+++ pages/maps.py
@@ -4,16 +4,16 @@ import dash_bootstrap_components as dbc
 import plotly.express as px
 
 #* map data dashboard
-map_data = pd.read_csv("../src/maps_statistics.csv")
-map_data = map_data.replace(',','', regex=True)
-map_data.loc[pd.to_numeric(map_data["Matches"]) < 8e3, "Map"] = 'Other maps'
-options = list(map_data["Map"])
+df = pd.read_csv("../src/maps_statistics.csv")
+df = df.replace(',','', regex=True)
+df.loc[pd.to_numeric(df["Matches"]) < 8e3, "Map"] = 'Other maps'
+options = list(df["Map"])
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 
 mapApp = Dash(__name__,external_stylesheets=external_stylesheets)
 
-fig = px.pie(map_data, values="Matches", names="Map", title="Map Play Rate", color_discrete_sequence=px.colors.sequential.Blues)
+fig = px.pie(df, values="Matches", names="Map", title="Map Play Rate", color_discrete_sequence=px.colors.sequential.Blues)
 fig.update_layout(paper_bgcolor='#092635', font_color="white")
 
 row = dbc.Row(
@@ -57,36 +57,36 @@ mapApp.layout = dbc.Container(
     Input('maps', 'value')
 )
 def update_output(value):
-    if value == map_data.iloc[0,0]:
-        val = [map_data.iloc[0,2].replace('%',''), map_data.iloc[0,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[1,0]:
-        val = [map_data.iloc[1,2].replace('%',''), map_data.iloc[1,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[2,0]:
-        val = [map_data.iloc[2,2].replace('%',''), map_data.iloc[3,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[3,0]:
-        val = [map_data.iloc[3,2].replace('%',''), map_data.iloc[3,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[4,0]:
-        val = [map_data.iloc[4,2].replace('%',''), map_data.iloc[4,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[5,0]:
-        val = [map_data.iloc[5,2].replace('%',''), map_data.iloc[5,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[6,0]:
-        val = [map_data.iloc[6,2].replace('%',''), map_data.iloc[6,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[7,0]:
-        val = [map_data.iloc[7,2].replace('%',''), map_data.iloc[7,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
-    elif value == map_data.iloc[8,0]:
-        val = [map_data.iloc[8,2].replace('%',''), map_data.iloc[8,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color=px.colors.sequential.Blues)
-    elif value == map_data.iloc[9,0]:
-        val = [map_data.iloc[9,2].replace('%',''), map_data.iloc[9,3].replace('%','')]
-        fig = px.pie(map_data, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color=px.colors.sequential.Blues)
+    if value == df.iloc[0,0]:
+        val = [df.iloc[0,2].replace('%',''), df.iloc[0,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[1,0]:
+        val = [df.iloc[1,2].replace('%',''), df.iloc[1,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[2,0]:
+        val = [df.iloc[2,2].replace('%',''), df.iloc[3,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[3,0]:
+        val = [df.iloc[3,2].replace('%',''), df.iloc[3,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[4,0]:
+        val = [df.iloc[4,2].replace('%',''), df.iloc[4,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[5,0]:
+        val = [df.iloc[5,2].replace('%',''), df.iloc[5,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[6,0]:
+        val = [df.iloc[6,2].replace('%',''), df.iloc[6,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[7,0]:
+        val = [df.iloc[7,2].replace('%',''), df.iloc[7,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color_discrete_sequence=px.colors.sequential.Blues)
+    elif value == df.iloc[8,0]:
+        val = [df.iloc[8,2].replace('%',''), df.iloc[8,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color=px.colors.sequential.Blues)
+    elif value == df.iloc[9,0]:
+        val = [df.iloc[9,2].replace('%',''), df.iloc[9,3].replace('%','')]
+        fig = px.pie(df, values=val, names=["T-Win %", "CT-Win %"], title=f"{value} Win Rate", color=px.colors.sequential.Blues)
     
     fig.update_layout(paper_bgcolor='#092635', font_color="white")
 
