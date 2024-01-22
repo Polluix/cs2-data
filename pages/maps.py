8734d751ed2d4bc3dbe55bbd0841e201f0e8bcc1
Polluix <lipe311973@gmail.com>
2024-01-22 10:38:03 -0300

    removed matplotlib and seaborn importations

--- pages/maps.py
+++ pages/maps.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 from dash import Dash, html, dash_table, dcc, Input, Output,callback
 import dash_bootstrap_components as dbc
 import plotly.express as px
