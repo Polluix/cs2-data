07a8de6efe9938494e35fe87f4a40aedf68c2763
Polluix <lipe311973@gmail.com>
2024-01-23 22:06:39 -0300

    included players per region histogram

--- pages/players.py
+++ pages/players.py
@@ -3,6 +3,7 @@ from dash import Dash, html, dash_table, dcc, Input, Output,callback
 import dash_bootstrap_components as dbc
 import plotly.express as px
 
+
 df = pd.read_csv('../src/top_100_players.csv')
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
@@ -14,24 +15,70 @@ playerApp = Dash(__name__,external_stylesheets=external_stylesheets)
 #* win rate
 #* win/loss por jogador
 
+player_df = pd.read_csv('../src/top_100_players.csv')
+player_df.drop("Name", axis='columns', inplace=True)
+player_df = player_df.replace(',','', regex=True)
+
+regions = player_df['Region'].unique()
+# region_count = player_df.value_counts('Region')
+
+region_count = px.histogram(player_df, x='Region', title='Number of players per region',
+                            color_discrete_sequence=px.colors.sequential.Blues, color='Region')
+
+region_count.update_layout(
+    {
+        'plot_bgcolor':'rgba(0,0,0,0)',
+        'paper_bgcolor':'rgba(0,0,0,0)',
+        'font_color':'white',
+    },
+    title={
+        'xanchor':'left',
+    },
+    showlegend=False
+)
+
+# print(region_count)
+# print(player_df)
+
+
+#2 rows and 2 collumns layout
 row1 = dbc.Row(
     [
-        dbc.Col(),
-        dbc.COl()
+        dbc.Col(
+            dcc.Graph(figure=region_count),
+            style={
+                'align':'center'
+            }
+        ),
+
+        dbc.Col(
+            
+        )
     ]
 )
 
 row2 = dbc.Row(
     [
-        dbc.Col(),
-        dbc.COl()
+        dbc.Col(
+            
+        ),
+        dbc.Col(
+            
+        )
     ]
 )
 
 playerApp.layout = dbc.Container(
-    row,
+    children=[
+        row1,
+        row2,
+    ],
     fluid=True,
-    style={'backgroundColor':'#092635'}
+    style={
+        'backgroundColor':'#092635',
+        'height':'100vh',
+        'width':'100vw'
+    }
 )
 
 if __name__=='__main__':
